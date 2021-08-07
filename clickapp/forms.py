045fc465ec721bb82315d4104178bdf2589a1373
leadmeout <markhorvat@protonmail.com>
2021-08-07 10:48:26 +0200

    read-only fields for tax_rate, gross and classification

--- clickapp/forms.py
+++ clickapp/forms.py
@@ -10,9 +10,14 @@ class TariffCalcForm(ModelForm):
         model = TariffCalc
         fields = '__all__'
 
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
         self.helper.form_show_labels = False
         self.helper.add_input(Submit('submit', 'Calculate'))
+
+        self.fields['tax_rate'].widget.attrs['readonly']=True
+        self.fields['gross'].widget.attrs['readonly']=True
+        self.fields['classification'].widget.attrs['readonly']=True
