3760e778a5eaa4cd2f0c3e4ac9b484faefe178ea
leadmeout <markhorvat@protonmail.com>
2021-08-07 10:59:44 +0200

    add result view

--- clickapp/views.py
+++ clickapp/views.py
@@ -6,3 +6,12 @@ def index(request):
     form = TariffCalcForm
     context = {'form': form}
     return render(request, 'clickapp/index.html', context)
+
+
+def result(request):
+    form = TariffCalcForm(request.POST)
+    context = {'form': form}
+    if form.is_valid():
+        return(render, 'clickapp/result.html', context)
+    else:
+        return render(request, 'clickapp/index.html', context)
