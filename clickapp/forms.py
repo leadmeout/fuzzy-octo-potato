219c2956b1ac48d3d01d94a160c48e1be8b18baa
leadmeout <markhorvat@protonmail.com>
2021-08-09 00:38:32 +0200

    ability to submit unfilled rows

--- clickapp/forms.py
+++ clickapp/forms.py
@@ -4,12 +4,10 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
 
-
 class TariffCalcForm(ModelForm):
     class Meta:
         model = TariffCalc
         fields = '__all__'
-
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,10 +16,9 @@ class TariffCalcForm(ModelForm):
         self.helper.form_show_labels = False
         self.helper.add_input(Submit('submit', 'Calculate'))
 
-        self.fields['tax_rate'].widget.attrs['readonly']=True
-        self.fields['gross'].widget.attrs['readonly']=True
-        self.fields['classification'].widget.attrs['readonly']=True
-
+        self.fields['tax_rate'].widget.attrs['readonly'] = True
+        self.fields['gross'].widget.attrs['readonly'] = True
+        self.fields['classification'].widget.attrs['readonly'] = True
 
     CLASSIFICATION_DICT = {
         'ch': 'EU',
@@ -42,27 +39,48 @@ class TariffCalcForm(ModelForm):
         0: 'Steuerfrei'
     }
 
-
     @property
     def get_tax_rate(self):
+
         try:
-            tax = self.INTERNAL_TAX_DICT[self.cleaned_data['customer']]
+            warehouse = self.cleaned_data['warehouse']
+            customer = self.cleaned_data['customer']
         except KeyError:
-            tax = self.INTERNAL_TAX_DICT[self.cleaned_data['warehouse']]
-        return self.TAX_RATE_DICT[tax]
+            return "------"
+
+        if customer:
+            try:
+                tax = self.INTERNAL_TAX_DICT[customer]
+            except KeyError:
+                if warehouse:
+                    tax = self.INTERNAL_TAX_DICT[warehouse]
+            finally:
+                return self.TAX_RATE_DICT[tax]
 
     @property
     def get_gross(self):
-        receiver = self.cleaned_data['customer']
-        net_total = float(self.cleaned_data['net'])
+
         try:
-            tax = float(self.INTERNAL_TAX_DICT[receiver])
+            receiver = self.cleaned_data['customer']
+            net_total = float(self.cleaned_data['net'])
         except KeyError:
-            tax = float(self.INTERNAL_TAX_DICT[self.cleaned_data['warehouse']])
-        gross_total = round(net_total * tax + net_total, 2)
-        return f"{gross_total:.2f}"
+            return "------"
+
+        if receiver and net_total:
+            try:
+                tax = float(self.INTERNAL_TAX_DICT[receiver])
+            except KeyError:
+                tax = float(
+                    self.INTERNAL_TAX_DICT[self.cleaned_data['warehouse']])
+            gross_total = round(net_total * tax + net_total, 2)
+            return f"{gross_total:.2f}"
 
     @property
     def get_classification(self):
-        receiver = self.cleaned_data['customer']
-        return self.CLASSIFICATION_DICT[receiver]
+        try:
+            receiver = self.cleaned_data['customer']
+            classification = self.CLASSIFICATION_DICT[receiver]
+        except KeyError:
+            return "------"
+        else:
+            return classification
