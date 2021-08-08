797fb44955205a9cea65bcfb1b714a1a53fc1cff
leadmeout <markhorvat@protonmail.com>
2021-08-08 23:33:22 +0200

    update views to use formsets

--- clickapp/views.py
+++ clickapp/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from .forms import TariffCalcForm
+from django.forms import modelformset_factory
+from .models import TariffCalc
+
 
 def index(request):
-    form = TariffCalcForm
-    context = {'form': form}
+
+    TariffCalcFormSet = modelformset_factory(TariffCalc, form=TariffCalcForm, extra=9)
+    formset = TariffCalcFormSet()
+    context = {'formset': formset}
     return render(request, 'clickapp/index.html', context)
 
 
 def result(request):
-    form = TariffCalcForm(request.POST)
-    context = {'form': form}
-    if form.is_valid():
-        return render(request, 'clickapp/result.html', context)
-    else:
-        return render(request, 'clickapp/index.html', context)
+
+    TariffCalcFormSet = modelformset_factory(TariffCalc, form=TariffCalcForm)
+    formset = TariffCalcFormSet(request.POST)
+    context = {'formset': formset}
+
+    for form in formset:
+        if form.is_valid():
+            return render(request, 'clickapp/result.html', context)
+        else:
+            return render(request, 'clickapp/index.html', context)
